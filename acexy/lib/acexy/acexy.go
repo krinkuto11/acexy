61dc1ccca609808fcfb231c8826d83f18efc0bb4
Ander Regidor <50609693+krinkuto11@users.noreply.github.com>
2025-07-13 13:50:08 +0200

    Added debug logs

--- acexy/lib/acexy/acexy.go
+++ acexy/lib/acexy/acexy.go
@@ -174,11 +174,17 @@ func (a *Acexy) StartStream(stream *AceStream, out io.Writer, username string) e
 	// Add the writer to the list of writers
 	ongoingStream.writers.Add(out)
 
+	//DEBUG
+	slog.Debug("Adding writer", "writer", out)
 	// Add the username
 	ongoingStream.users[out] = username
-	
+	//DEBUG
+	slog.Debug("Tracking user", "username", username, "writer", out)
+
 	// Register the new client
 	ongoingStream.clients++
+	//DEBUG
+	slog.Debug("Registered client", "clients", ongoingStream.clients)
 
 
 
@@ -440,6 +446,8 @@ func (a *Acexy) GetStatus(id *AceID) (AcexyStatus, error) {
 	if stream, ok := a.streams[*id]; ok {
 		users := make([]string, 0, len(stream.users))
 		for _, user := range stream.users {
+			//DEBUG
+			slog.Debug("Listing user", "user", user)
 			users = append(users, user)
 		}
 		return AcexyStatus{
